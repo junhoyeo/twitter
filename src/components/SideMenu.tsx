8a184409f1b4582cf91398e82e3ff06ebcd44a49
Junho Yeo <hanaro0704@gmail.com>
2021-09-20 05:34:27 +0900

    feat: implement `SideMenu`

---
+++ src/components/SideMenu.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import styled from 'styled-components';
+import TwitterLogoIcon from '../pages/twitter.svg';
+
+import HomeSolidIcon from '../assets/sidemenu/home-solid.svg';
+import HomeOutlineIcon from '../assets/sidemenu/home-outline.svg';
+import ExploreSolidIcon from '../assets/sidemenu/explore-solid.svg';
+import ExploreOutlineIcon from '../assets/sidemenu/explore-outline.svg';
+import BookmarksSolidIcon from '../assets/sidemenu/bookmarks-solid.svg';
+import BookmarksOutlineIcon from '../assets/sidemenu/bookmarks-outline.svg';
+import ProfileSolidIcon from '../assets/sidemenu/profile-solid.svg';
+import ProfileOutlineIcon from '../assets/sidemenu/profile-outline.svg';
+import { useRouter } from 'next/dist/client/router';
+import { SideMenuItem } from './SideMenuItem';
+
+const sideMenuItems = [
+  {
+    name: 'Home',
+    path: '/',
+    solidIcon: <HomeSolidIcon />,
+    outlineIcon: <HomeOutlineIcon />,
+  },
+  {
+    name: 'Explore',
+    path: '/explore',
+    solidIcon: <ExploreSolidIcon />,
+    outlineIcon: <ExploreOutlineIcon />,
+  },
+  {
+    name: 'Bookmarks',
+    path: '/bookmarks',
+    solidIcon: <BookmarksSolidIcon />,
+    outlineIcon: <BookmarksOutlineIcon />,
+  },
+  {
+    name: 'Profile',
+    path: '/profile',
+    solidIcon: <ProfileSolidIcon />,
+    outlineIcon: <ProfileOutlineIcon />,
+  },
+];
+
+export const SideMenu = () => {
+  const { asPath: pathname } = useRouter();
+
+  return (
+    <Wrapper>
+      <Container>
+        <TwitterLogoContainer>
+          <TwitterLogo />
+        </TwitterLogoContainer>
+        <SidemenuList>
+          {sideMenuItems.map((props) => {
+            const selected = pathname === props.path;
+            return (
+              <SideMenuItem key={props.path} selected={selected} {...props} />
+            );
+          })}
+        </SidemenuList>
+      </Container>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.header`
+  flex-grow: 1;
+  justify-content: flex-end;
+  display: flex;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 275px;
+  padding: 0 12px;
+
+  list-style-type: none;
+  position: sticky;
+  top: 0;
+
+  @media (max-width: 1200px) {
+    width: fit-content;
+  }
+`;
+
+const TwitterLogoContainer = styled.div`
+  width: 52px;
+  height: 52px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const TwitterLogo = styled(TwitterLogoIcon)`
+  fill: rgb(217, 217, 217);
+  width: 50px;
+  height: 30px;
+`;
+
+const SidemenuList = styled.ul`
+  margin: 0;
+  margin-top: 2px;
+  padding: 0;
+`;
---
+++ src/components/SideMenuItem.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import Link from 'next/link';
+
+type Props = {
+  name: string;
+  path: string;
+  selected: boolean;
+  solidIcon: React.ReactNode;
+  outlineIcon: React.ReactNode;
+};
+
+export const SideMenuItem: React.FC<Props> = ({
+  name,
+  path,
+  selected,
+  solidIcon,
+  outlineIcon,
+}) => {
+  return (
+    <Link href={path}>
+      <Container selected={selected}>
+        {!selected ? outlineIcon : solidIcon}
+        <span>{name}</span>
+      </Container>
+    </Link>
+  );
+};
+
+type ContainerProps = {
+  selected: boolean;
+};
+const Container = styled.li<ContainerProps>`
+  display: flex;
+  align-items: center;
+  min-width: 100%;
+  margin-left: auto;
+  border-radius: 36px;
+  padding: 12px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  list-style-type: none;
+
+  @media (max-width: 1400px) {
+    min-width: 220px;
+  }
+
+  @media (max-width: 1200px) {
+    min-width: fit-content;
+    padding: 12x;
+    justify-content: center;
+  }
+
+  & > * {
+    transition: all 0.1s ease-in-out;
+  }
+
+  & svg {
+    width: 26.25px;
+    height: 26.25px;
+    fill: rgba(217, 217, 217, 1);
+  }
+
+  span {
+    margin-left: 20px;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 24px;
+    color: #d9d9d9;
+
+    @media (max-width: 1200px) {
+      display: none;
+    }
+  }
+
+  &:hover {
+    background-color: rgba(217, 217, 217, 0.1);
+  }
+
+  ${({ selected }) =>
+    selected &&
+    css`
+      span {
+        font-weight: 700;
+      }
+    `};
+`;
