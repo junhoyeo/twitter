3849f4f47e8dcc6487507b4ba4d854c7cd7776ce
Junho Yeo <hanaro0704@gmail.com>
2021-09-21 06:30:14 +0900

    feat: show search input in right sidebar

--- src/components/Layout/RightSideBar.tsx
+++ src/components/Layout/RightSideBar.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import SearchIcon from '../../assets/search.svg';
 import { TrendsForYou } from './TrendsForYou';
 
 type Props = React.HTMLAttributes<HTMLDivElement>;
@@ -7,6 +8,12 @@ type Props = React.HTMLAttributes<HTMLDivElement>;
 export const RightSideBar: React.FC<Props> = (props) => {
   return (
     <Container {...props}>
+      <Sticky>
+        <SearchBarContainer>
+          <SearchIcon />
+          <SearchBarInput placeholder="Search Twitter" />
+        </SearchBarContainer>
+      </Sticky>
       <TrendsForYou />
     </Container>
   );
@@ -16,4 +23,56 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
+`;
+
+const Sticky = styled.div`
+  margin-bottom: 12px;
+  width: 100%;
+  height: 53px;
+  min-height: 32px;
+  background-color: black;
+
+  position: sticky;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+
+  display: flex;
+`;
+const SearchBarContainer = styled.div`
+  margin: auto;
+  width: 100%;
+  height: 42px;
+
+  display: flex;
+  align-items: center;
+
+  border: 1px solid rgba(0, 0, 0, 0);
+  border-radius: 99px;
+  background-color: rgb(32, 35, 39);
+
+  & > svg {
+    padding-left: 12px;
+    fill: rgb(110, 118, 125);
+    width: 32px;
+    min-width: 44px;
+    height: 18.75px;
+  }
+`;
+const SearchBarInput = styled.input`
+  padding: 12px;
+  width: 100%;
+
+  border: 0;
+  background-color: transparent;
+
+  font-size: 15px;
+  font-weight: 400;
+  line-height: 20px;
+  color: rgb(217, 217, 217);
+
+  &::placeholder {
+    color: rgb(110, 118, 125);
+  }
 `;
